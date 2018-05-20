01b245efdd30c5fe6a7a0239d124129e41135d51
Suchitra Naik <susanaik@gmail.com>
2018-05-20 12:26:56 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -52,7 +52,7 @@ class Student
     end
 
     def self.new_from_db(row)
-      new_student = self.new(:id = row[0], :name = row[1], :grade = row[2])
+      new_student = self.new(row[1], row[2],row[0])
 
       new_student
     end
